a894da4d29949c25d982ff7305b34c4dac4bbfe1
a <a>
2025-01-03 00:18:55 +0300

    delete config

--- cmd/main.go
+++ cmd/main.go
@@ -18,9 +18,9 @@ func main() {
 	var (
 		formFlag       = flag.String("f", availableForms[0], "Available forms: "+strings.Join(availableForms, ", "))
 		pathFlag       = flag.String("p", ".", "path")
+		filetypeFlag   = flag.String("ft", "", "count files with file type")
 		timeFlag       = flag.Bool("t", false, "benchmark")
 		showHiddenFlag = flag.Bool("h", false, "show hidden files")
-		filetypeFlag   = flag.String("ft", "", "count files with file type")
 		fileSizeFlag   = flag.Bool("s", false, "count size of files")
 	)
 	flag.Parse()
