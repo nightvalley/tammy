1829a6385eaed8357cbc3f7d5b4ecf1d84c66ee9
a <a>
2025-01-02 17:33:21 +0300

    list fix

--- internal/forms/list.go
+++ internal/forms/list.go
@@ -12,30 +12,24 @@ func ListOutput(expandedPath string, flags files.Flags) {
 	f := files.Files{}
 	f.FoundAllFilesInDir(expandedPath, flags)
 
-	if !flags.ShowSize {
-		l := list.New()
-		for i, name := range f.Name {
-			nameAndLines := fmt.Sprintf("%s: %d lines", filepath.Base(name), f.Lines[i])
-			l.Item(nameAndLines)
-		}
-		l.Item("Total lines: " + fmt.Sprintf("%d", f.TotalLines))
+	l := list.New()
+	for i, name := range f.Name {
+		addFileInfoToList(l, name, f.Lines[i], f.Size[i], flags.ShowSize)
+	}
+	l.Item("Total lines: " + fmt.Sprintf("%d", f.TotalLines))
+	l.Enumerator(list.Roman)
 
-		l.Enumerator(list.Roman)
+	fmt.Println(l)
+}
 
-		fmt.Println(l)
+func addFileInfoToList(l *list.List, name string, lines int, size files.FileSize, showSize bool) {
+	if showSize {
+		nameAndLines := fmt.Sprintf("%s: %d lines, %.2f %s",
+			filepath.Base(name), lines,
+			size.Size, size.Unit)
+		l.Item(nameAndLines)
 	} else {
-		l := list.New()
-		for i, name := range f.Name {
-			size := f.Size[i]
-			nameAndLines := fmt.Sprintf("%s: %d lines, %.2f %s",
-				filepath.Base(name), f.Lines[i],
-				size.Size, size.Unit)
-			l.Item(nameAndLines)
-		}
-		l.Item("Total lines: " + fmt.Sprintf("%d", f.TotalLines))
-
-		l.Enumerator(list.Roman)
-
-		fmt.Println(l)
+		nameAndLines := fmt.Sprintf("%s: %d lines", filepath.Base(name), lines)
+		l.Item(nameAndLines)
 	}
 }
