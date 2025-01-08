e4ffe18f810f6f46b6f1a2524df874896aacf956
a <a>
2025-01-08 21:41:34 +0300

    first version

--- internal/checkversion.go
+++ internal/checkversion.go
@@ -11,7 +11,7 @@ type Release struct {
 }
 
 func CheckForUpdates() (bool, error) {
-	currentVersion := "v2.0.1"
+	currentVersion := "v1.0.0"
 
 	repo := "nightvalley/tammy"
 	url := fmt.Sprintf("https://api.github.com/repos/%s/releases/latest", repo)
