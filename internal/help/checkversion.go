d9fdde0e397e686832a684c8b5f946d2b7a43fff
nightvalley <barges_wilts.87@icloud.com>
2025-01-15 10:49:03 +0300

    correct version

--- internal/help/checkversion.go
+++ internal/help/checkversion.go
@@ -11,7 +11,7 @@ type Release struct {
 }
 
 func CheckForUpdates() (bool, error) {
-	currentVersion := "v1.2.2"
+	currentVersion := "v1.2.0"
 
 	repo := "nightvalley/tammy"
 	url := fmt.Sprintf("https://api.github.com/repos/%s/releases/latest", repo)
